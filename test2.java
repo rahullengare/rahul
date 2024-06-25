6a1c3d02a3cb5ec9e94c9345f0b72c5fe28127d3
Rahul <lengarerahul5@gmail.com>
2024-06-25 12:15:47 +0530

    github

---
+++ test2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Test 
+{
+	public static void main(String args[])
+	{
+	   	TreeSet hset=new TreeSet();
+		hset.add(40);
+		hset.add(20);
+		hset.add(30);
+		hset.add(50);
+		hset.add(60);
+		System.out.println(hset);
+	}
+}
+
+
